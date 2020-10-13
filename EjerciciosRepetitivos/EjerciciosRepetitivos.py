df341df82d564055eef4bc9d97ef281c6c97153f
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-13 14:31:44 +0000

    EjemploFor

--- EjerciciosRepetitivos/EjerciciosRepetitivos.py
+++ EjerciciosRepetitivos/EjerciciosRepetitivos.py
@@ -1,10 +1,9 @@
-def suma10NumerosWhile():
+def suma10NumerosFor():
     #Definiendo variables e inicializando variables
-    nContadorCondicion,numero, suma=1,0,0
+    numero, suma=0,0
     #Proceso y datos de entrada
-    while nContadorCondicion<=10:
+    for nContadorCondicion in range(1,11):
         numero=float(input(f"Ingrese el numero de la posicion  {nContadorCondicion} :"))
         suma=suma+numero
-        nContadorCondicion=nContadorCondicion+1
     #Datos de salida
     print(f"La suma de los 10 numeros ingresados es: {suma}")
