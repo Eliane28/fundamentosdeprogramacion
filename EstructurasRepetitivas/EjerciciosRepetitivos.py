707692ad4886d4381148d0167f353e2dfba24453
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-13 14:00:27 +0000

    EjerciciosRepetitivos

---
+++ EstructurasRepetitivas/EjerciciosRepetitivos.py
@@ -0,0 +1,10 @@
+def suma10numeros():
+    #Definiendo variables e iniciando variables 
+    ncontadorcondicion,numero,suma=0,0,0
+    #proceso y datos de entrada 
+    while ncontadorcondicion<-0:
+        numero = float( input (f " ingrese el numero de la posición {ncontadorcondicion}:"))
+        suma = suma + nuemro 
+        ncontadorcondicon-ncontadorcondicon+1
+        print (f"la suma de los 10 numeros ingresados es {suma}"")
+        suma10numeros()
