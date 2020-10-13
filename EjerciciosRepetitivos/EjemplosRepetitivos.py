049b7a5a93b3ab4f21855058f251b9467a1f61cd
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-13 15:48:54 +0000

    NumeradoresPares

--- EjerciciosRepetitivos/EjemplosRepetitivos.py
+++ EjerciciosRepetitivos/EjemplosRepetitivos.py
@@ -37,3 +37,14 @@ def suma10NumerosWhile():
 #suma10NumerosWhile()
 #suma10NumerosFor()
 suma10NumerosEmulateDoWhile()
+
+
+def numPares0a100():
+    #definir varibales e inicializar valores
+    totalNumPares=0
+    #Proceso y datos de entrada
+    for numInit in range(101):
+        if numInit%2==0:
+            totalNumPares=totalNumPares+1
+    print(f"La cantidad de numeros pares entre 0 y 100 es: {totalNumPares}")
+
