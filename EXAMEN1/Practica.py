df13192514c47e0318c090386bcc1416272c6753
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-14 19:32:31 +0000

    CalificacionF

---
+++ EXAMEN1/Practica.py
@@ -0,0 +1,11 @@
+def Calificacion():
+     n1=int(input("Nota de primera unidad:"))
+     n2=int(input("Nota de primera unidad:"))
+     n3=int(input("Nota de primera unidad:"))
+     n4=int(input("Nota de primera unidad:"))
+    #proceso 
+    pc=(n1+n2+n3+n4)/4
+    #datos de salida
+    print("la nota final es : ",pc) 
+
+promedio_de_la_nota()
