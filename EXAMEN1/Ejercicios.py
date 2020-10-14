3f6b5bd49e4b1befde7541e1ffa12e364a3feb5a
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-14 19:11:09 +0000

    calificaciónF

---
+++ EXAMEN1/Ejercicios.py
@@ -0,0 +1,9 @@
+def Calificacion():
+    n1=int(input("Nota de primera unidad:"))
+    n2=int(input("Nota de segunda unidad:"))
+    n3=int(input("Nota de tercera unidad:"))
+    n4=int(input("Nota de trabajo final:"))
+    #proceso
+    pc=(n1+n2+n3+n4)/4
+    #datos de salida 
+    print("La nota final del alunmo es:",nf)  
