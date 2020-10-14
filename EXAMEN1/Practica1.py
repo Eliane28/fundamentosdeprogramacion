24309d5555f3ac66db57d71fc931895c51da8623
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-14 21:58:48 +0000

    Calificacion

---
+++ EXAMEN1/Practica1.py
@@ -0,0 +1,11 @@
+def Calificacion():
+    #datos de entrada 
+     n1=int(input("Nota de primera unidad:"))
+     n2=int(input("Nota de primera unidad:"))
+     n3=int(input("Nota de primera unidad:"))
+     n4=int(input("Nota de primera unidad:"))
+    #proceso 
+    Cl=(n1+n2+n3+n4)/4
+    #datos de salida
+    print("la calificacion es :",Cl) 
+CalificacionFinal()
