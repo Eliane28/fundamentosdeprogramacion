6a2540a73325d05aabd07a4121651161b5fff3f4
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-14 20:42:59 +0000

    Monto del bono del profesor

--- EXAMEN1/Practica.py
+++ EXAMEN1/Practica.py
@@ -9,3 +9,22 @@ def Calificacion():
     print("la nota final es : ",pc) 
 
 promedio_de_la_nota()
+
+def Monto_bono_del_profesor():
+    # #Definiendo variables e inicializando variables
+    monto,salario,puntos=0
+    puntos=int(input("Digite el valor de puntos:"))
+    salario=int(input("Digite el salario minimo:"))
+    #Proceso y datos de entrada
+    if  puntos >= 50:
+    monto <- salario * 0.1
+    if puntos <= 100:
+    monto <- salario * 0.1
+    if  puntos >=101 y puntos <= 150:
+    monto <- salario * 0.50
+    if puntos >=151:
+    monto <- salario * 0.8
+    #datos de salida 
+    print("El monto del bono del profesor es:",monto)
+
+
