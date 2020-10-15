571acc7a0bbde71c75e212e4ab240c5e550a750e
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-15 12:42:09 +0000

    CalcularPintado

--- EstructuraSecuencial/EjemplosSecuencialD.py
+++ EstructuraSecuencial/EjemplosSecuencialD.py
@@ -8,4 +8,12 @@ def CalcularPromedioNota():
     PC = (N1+N2+N3+N4)/4
     #Dato de salida
     print(f'El promedio de la calificacion es: {PC}')
-    
+
+def CalcularElPintado():
+     #Datos de Entrada
+    cantidadM2= int(input('Ingrese la cantidad m2 a pintar: '))
+    costoM2 = int(input('Costo por m2 : '))
+    #Proceso
+    montoC = cantidadM2 * costoM2
+    #Dato de salida
+    print(f'Monto a cobrar es : {montoC}')    
