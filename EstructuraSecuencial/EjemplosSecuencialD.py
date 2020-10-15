c085a1eef1634992da9dc1754ba53c0df940c0dd
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-15 12:40:42 +0000

    Nota

---
+++ EstructuraSecuencial/EjemplosSecuencialD.py
@@ -0,0 +1,11 @@
+def CalcularPromedioNota():
+    #Datos de Entrada
+    N1 = int(input('Ingrese Nota 1: '))
+    N2 = int(input('Ingrese Nota 2: '))
+    N3 = int(input('Ingrese Nota 3: '))
+    N4 = int(input('Ingrese Nota 4: '))
+    #Proceso
+    PC = (N1+N2+N3+N4)/4
+    #Dato de salida
+    print(f'El promedio de la calificacion es: {PC}')
+    
