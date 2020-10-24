3182fca40fdc15fa1c1fcc99a7be313f6c95efec
Eliane28 <elianrivera@upeu.edu.pe>
2020-10-24 02:36:00 +0000

    Ejemplo Factorial de Java, Java Script

---
+++ EstructurasRepetitivas/FactorialIEjava.py
@@ -0,0 +1,46 @@
+package EstRepetitivas;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FactorialE{
+
+    static Scanner cs=new Scanner(System.in);
+
+
+    public static void calcularFactorial(int numero){
+        int contador=1;
+        long resultado=1;
+        if(numero>1){resultado=numero;}
+        while(contador<numero){
+            resultado=resultado*contador;
+            contador++;
+        }
+        System.out.println("El factorial de "+numero+" es "+resultado+"");
+    }
+
+    public static void calcularFactorialBig(int numero) {
+        int contador=1;
+        BigInteger resultado=BigInteger.valueOf(1);
+
+        if(numero>1){resultado=BigInteger.valueOf(numero);}
+        while(contador<numero){
+            resultado=resultado.multiply(BigInteger.valueOf(contador));
+            contador++;
+        }
+        System.out.println("El factorial de "+numero+" es "+resultado+"");
+    }
+
+    public static void rangoFactorial(int numfinal) {
+        for(int inicio=0;inicio<=numfinal;inicio++){
+            calcularFactorialBig(inicio);
+        }
+    }
+
+    public static void main(String[] args) {        
+        rangoFactorial(cs.nextInt());
+    }
+}
+© 2020 GitHub, Inc.
+Terms
+Privacy
